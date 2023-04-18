9e05fd0f7043e336dc6ca4386f092f64c9fcf34f
jchan6532 <82909011+jchan6532@users.noreply.github.com>
2023-04-18 01:37:11 -0400

    added constants class and vehicle type enumeration

---
+++ Constants/VehicleTpe.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Constants
+{
+    /// <summary>
+    /// Vehicle Type enumeration
+    /// </summary>
+    enum VehicleType
+    {
+        /// <summary>
+        /// Sedan
+        /// </summary>
+        Sedan,
+
+        /// <summary>
+        /// HatchBack
+        /// </summary>
+        HatchBack,
+
+        /// <summary>
+        /// SUV
+        /// </summary>
+        SUV,
+
+        /// <summary>
+        /// Coupe
+        /// </summary>
+        Coupe,
+
+        /// <summary>
+        /// Pickup Truck
+        /// </summary>
+        PickupTruck,
+
+        /// <summary>
+        /// Truck
+        /// </summary>
+        Truck,
+
+        /// <summary>
+        /// Motorcycle
+        /// </summary>
+        Motorcycle,
+
+        /// <summary>
+        /// MotorBike
+        /// </summary>
+        MotorBike
+    }
+}
