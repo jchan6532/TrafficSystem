ce83776ee55c2bd5cf0fc226cb0ba058855b8997
Justin.Chan <Justin.Chan@peergroup.com>
2023-04-20 11:15:59 -0400

    made conneciton string private

--- Database/DatabaseConnecter.cs
+++ Database/DatabaseConnecter.cs
@@ -12,17 +12,17 @@ namespace Database
     /// <summary>
     /// Database Connecter
     /// </summary>
-    public class DatabaseConnecter : IQueries
+    public class DatabaseConnecter
     {
+        /// <summary>
+        /// Connection String
+        /// </summary>
+        private string ConnectionString = null;
+
         /// <summary>
         /// SQL connection
         /// </summary>
         public MySqlConnection Connection { get; set; } = null;
-
-        /// <summary>
-        /// Connection String
-        /// </summary>
-        public string ConnectionString { get; set; } = null;
 
         /// <summary>
         /// Data table
@@ -37,7 +37,7 @@ namespace Database
         /// <summary>
         /// SQL Command
         /// </summary>
-        public MySqlCommand Command { get; set; }
+        public MySqlCommand Command { get; private set; } = null;
 
         /// <summary>
         /// Default Constructor
@@ -46,22 +46,41 @@ namespace Database
         {
             try
             {
-                string user = ConfigurationSettings.AppSettings.Get("UserName");
-                string password = ConfigurationSettings.AppSettings.Get("Password");
-                string databaseName = ConfigurationSettings.AppSettings.Get("Database");
-                string ip = ConfigurationSettings.AppSettings.Get("DatabaseIP");
-                string port = ConfigurationSettings.AppSettings.Get("DatabasePort");
+                string user = ConfigurationManager.AppSettings.Get("UserName");
+                string password = ConfigurationManager.AppSettings["Password"];
+                string databaseName = ConfigurationManager.AppSettings["Database"];
+                string ip = ConfigurationManager.AppSettings["DatabaseIP"];
+                string port = ConfigurationManager.AppSettings["DatabasePort"];
+
+                this.ConnectionString = $"user={user}; password={password}; database={databaseName}; server={ip};";
 
                 this.Connection = new MySqlConnection(this.ConnectionString);
                 this.Connection.Open();
 
-                this.Adapter = new MySqlDataAdapter(this.Command);
-                this.Adapter.Fill(this.Table);
+                //this.Adapter = new MySqlDataAdapter(this.Command);
+                //this.Adapter.Fill(this.Table);
             }
             catch (Exception e)
             {
                 throw e;
             }
+        }
+
+        /// <summary>
+        /// Runs the query.
+        /// </summary>
+        public int RunQuery(string query)
+        {
+            if (this.Command == null)
+            {
+                this.Command = new MySqlCommand(string.Empty, this.Connection);
+            }
+
+            this.Command.CommandText = query;
+
+            int numRows = this.Command.ExecuteNonQuery();
+
+            return numRows;
         }
     }
 }
